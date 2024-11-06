553eecaebe71d5a2e83ab944fc18f6deebb57c91
miit-study-easy <166275802+miit-study-easy@users.noreply.github.com>
2024-11-06 22:38:45 +0630

    Update Player.lua

--- Player.lua
+++ Player.lua
@@ -1,6 +1,9 @@
 player = {}
 
 function player.load()
+  wf = require 'libraries/windfield'
+  world = wf.newWorld(0, 0)
+
   camera = require 'libraries/camera'
   cam = camera()
 
@@ -11,9 +14,11 @@ function player.load()
   gameMap = sti('maps/testMap.lua')
   
   player = {}
+  player.collider = world:newBSGRectangleCollider(400, 200, 25, 50, 5)
+  player.collider:setFixedRotation(true)
   player.x = 400
   player.y = 200
-  player.speed = 5
+  player.speed = 300
   player.spriteSheet = love.graphics.newImage("res/Girl_Sprite-Sheet.png")
   player.grid = anim8.newGrid(64, 64, player.spriteSheet:getWidth(), player.spriteSheet:getHeight())
 
@@ -24,39 +29,58 @@ function player.load()
   player.animations.up = anim8.newAnimation(player.grid('1-4', 4), 0.2)
 
   player.anim = player.animations.left --to track player animation
+
+  walls = {}
+  if gameMap.layers["walls"] then
+    for i, obj in pairs(gameMap.layers["walls"].objects) do
+      local wall = world:newRectangleCollider(obj.x, obj.y, obj.width, obj.height)
+      wall:setType('static')
+      table.insert(walls, wall)
+    end
+  end
   
 end
 
 function player.update(dt)
   local isMoving = false
+
+  local vx = 0
+  local vy = 0
   
   if love.keyboard.isDown("right") then
-    player.x = player.x + player.speed
+    vx =  player.speed
     player.anim = player.animations.right
     isMoving = true
   end
 
   if love.keyboard.isDown("left") then
-    player.x = player.x - player.speed
+    vx = player.speed * -1
     player.anim = player.animations.left
     isMoving = true
   end
 
   if love.keyboard.isDown("down") then
-    player.y = player.y + player.speed
+    vy = player.speed
     player.anim = player.animations.down
     isMoving = true
   end
 
   if love.keyboard.isDown("up") then
-    player.y = player.y - player.speed
+    vy = player.speed * -1
     player.anim = player.animations.up
     isMoving = true
   end
 
+  player.collider:setLinearVelocity(vx, vy)
+
   if isMoving == false then
     player.anim:gotoFrame(1) --go to standing still frame -> column number
   end
+
+  --collisions
+  world:update(dt)
+  player.x = player.collider:getX()
+  player.y = player.collider:getY()
 
   player.anim:update(dt)
 
@@ -93,7 +117,7 @@ function player.draw()
     gameMap:drawLayer(gameMap.layers["Tile Layer 1"])
     gameMap:drawLayer(gameMap.layers["road"])
     gameMap:drawLayer(gameMap.layers["door"])
-    player.anim:draw(player.spriteSheet, player.x, player.y, nil, 1.5, nil, 0.25, 0.3 ) --posx, posy, nil-> no rotation, scale x factor-> y wil also adopt that effect
+    player.anim:draw(player.spriteSheet, player.x, player.y, nil, 1.5, nil, 32, 32) --posx, posy, nil-> no rotation, scale x factor-> y wil also adopt that effect
     --offset of camera must take half of width and half of height of sprite (to go directly in the center)
   cam:detach()
 end
