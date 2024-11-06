6f6b003b54dbe08ae22d1b2738693b8d0ad1e834
miit-study-easy <166275802+miit-study-easy@users.noreply.github.com>
2024-11-06 14:35:28 +0630

    Update Player.lua

--- Player.lua
+++ Player.lua
@@ -2,13 +2,13 @@ player = {}
 
 function player.load()
   camera = require 'libraries/camera'
-  cam = camera() --camera to follow sprite
-  
-  anim8 = requrie 'libraries/anim8'
+  cam = camera()
+
+  anim8 = require 'libraries/anim8'
   love.graphics.setDefaultFilter("nearest", "nearest") --dun do blurring when we scale the sprite
 
   sti = require 'libraries/sti'
-  gameMap = sti('maps/testMap.lua') --map setup
+  gameMap = sti('maps/testMap.lua')
   
   player = {}
   player.x = 400
@@ -59,7 +59,33 @@ function player.update(dt)
   end
 
   player.anim:update(dt)
+
   cam:lookAt(player.x, player.y)
+
+  -- only use after resizing sprite and map
+
+  -- local w = love.graphics.getWidth()
+  -- local h = love.graphics.getHeight()
+
+  -- if cam.x < w/2 then
+  --   cam.x = w/2
+  -- end
+
+  -- if cam.y < h/2 then
+  --   cam.y = h/2
+  -- end
+
+  -- local mapW = gameMap.width * gameMap.tilewidth
+  -- local mapH = gameMap.height * gameMap.tileheight
+
+  -- if cam.x > (mapW - w/2) then
+  --   cam.x = (mapW - w/2)
+  -- end
+
+  -- if cam.y > (mapH - h/2) then
+  --   cam.y = (mapH - h/2)
+  -- end
+  
 end
 
 function player.draw()
