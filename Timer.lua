fe3195aa1bb5aa7875a4bf83bf3cb441ed0cdd03
miit-study-easy <166275802+miit-study-easy@users.noreply.github.com>
2024-11-09 12:11:55 +0630

    Update Timer.lua

--- Timer.lua
+++ Timer.lua
@@ -28,16 +28,27 @@ function timer.update(dt)
 end
 
 function timer.draw()
-    -- Set text color to white
-    -- love.graphics.setColor(1, 1, 1)
-
+    -- Save the current transformation
+    love.graphics.push()
     
+    -- Reset the transformation to make the position fixed in the window
+    love.graphics.origin()
+    
+    -- Set text color to white
+    love.graphics.setColor(1, 1, 1)
+
+    -- Draw the timer text at a fixed position in the window
     love.graphics.printf("Time Remaining: " .. math.ceil(timeRemaining), 50, 50, love.graphics.getWidth(), "left")
+    
     -- If the timer has finished, display a "Time's Up!" message
     if not isTimerRunning then
-        love.graphics.printf("Time's Up!", 50, 50, 100)
+        love.graphics.printf("Time's Up!", 50, 80, love.graphics.getWidth(), "left")
     end
+
+    -- Restore the previous transformation
+    love.graphics.pop()
 end
+
 
 
 return timer
