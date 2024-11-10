24f23cf5b2cc0e32d1f2ff09d179020490bbb2cc
Wai Yan Htut <waiyanhtut354@gmail.com>
2024-11-10 23:06:31 +0630

    seperated the animation function

--- Player.lua
+++ Player.lua
@@ -1,6 +1,8 @@
-player = {}
 local gameMap = require("GameMap")
+local anim8 = require 'libraries/anim8'
+local animate = require("helpers.animationHelper")
 
+player = {}
 player.isAttacking = false
 player.isMoving = false
 
@@ -11,29 +13,29 @@ function player.load()
   cam = camera()
 
   anim8 = require 'libraries/anim8'
-  love.graphics.setDefaultFilter("nearest", "nearest") --dun do blurring when we scale the sprite
-  
+  love.graphics.setDefaultFilter("nearest", "nearest") -- dun do blurring when we scale the sprite
+
   player.collider = gameMap.world:newBSGRectangleCollider(1000, 1000, 25, 50, 5)
   player.collider:setFixedRotation(true)
-  player.x = 400
-  player.y = 200
+  player.x, player.y = 400, 200
   player.speed = 300
   player.spriteSheet = love.graphics.newImage("res/Girl_Sprite-Sheet.png")
   player.grid = anim8.newGrid(64, 64, player.spriteSheet:getWidth(), player.spriteSheet:getHeight())
 
-  player.animations = {}
-  player.animations.down = anim8.newAnimation( player.grid('1-4', 1), 0.2) --column 1 to 4, row 1, frame rate
-  player.animations.left = anim8.newAnimation( player.grid('1-4', 2), 0.2)
-  player.animations.right = anim8.newAnimation(player.grid('1-4', 3), 0.2)
-  player.animations.up = anim8.newAnimation(player.grid('1-4', 4), 0.2)
-  player.animations.downatk = anim8.newAnimation(player.grid('1-4', 5), 0.1)
-  player.animations.leftatk = anim8.newAnimation(player.grid('1-4', 6), 0.1)
-  player.animations.rightatk = anim8.newAnimation(player.grid('1-4', 7), 0.1)
-  player.animations.upatk = anim8.newAnimation(player.grid('1-4', 8), 0.1)
+  player.animations = {
+    down = animate(player.grid, '1-4', 1, 0.2), -- column 1 to 4, row 1, frame rate
+    left = animate(player.grid, '1-4', 2, 0.2),
+    right = animate(player.grid, '1-4', 3, 0.2),
+    up = animate(player.grid, '1-4', 4, 0.2),
+    downatk = animate(player.grid, '1-4', 5, 0.1),
+    leftatk = animate(player.grid, '1-4', 6, 0.1),
+    rightatk = animate(player.grid, '1-4', 7, 0.1),
+    upatk = animate(player.grid, '1-4', 8, 0.1)
+  }
 
-  player.anim = player.animations.left --to track player animation
+  player.anim = player.animations.left -- to track player animation
   player.attack = "left"
-  
+
 end
 
 function player.update(dt)
@@ -41,9 +43,9 @@ function player.update(dt)
 
   local vx = 0
   local vy = 0
-  
+
   if love.keyboard.isDown("right") then
-    vx =  player.speed
+    vx = player.speed
     player.anim = player.animations.right
     player.attack = 'right'
     player.isMoving = true
@@ -70,12 +72,12 @@ function player.update(dt)
     player.isMoving = true
   end
 
-    if love.keyboard.isDown("a") then
+  if love.keyboard.isDown("a") then
     -- player.isMoving = true
     if player.attack == 'down' then
       player.anim = player.animations.downatk
       player.isAttacking = true
-    
+
     elseif player.attack == 'left' then
       player.anim = player.animations.leftatk
       player.isAttacking = true
@@ -92,7 +94,7 @@ function player.update(dt)
 
   -- Normalize the vector if moving diagonally
   if vx ~= 0 and vy ~= 0 then
-    local magnitude = math.sqrt(vx^2 + vy^2)
+    local magnitude = math.sqrt(vx ^ 2 + vy ^ 2)
     vx = (vx / magnitude) * player.speed
     vy = (vy / magnitude) * player.speed
   end
@@ -100,10 +102,10 @@ function player.update(dt)
   player.collider:setLinearVelocity(vx, vy)
 
   if player.isMoving == false then
-    player.anim:gotoFrame(1) --go to standing still frame -> column number
+    player.anim:gotoFrame(1) -- go to standing still frame -> column number
   end
 
-  --collisions
+  -- collisions
   gameMap.update(dt)
   player.x = player.collider:getX()
   player.y = player.collider:getY()
@@ -117,32 +119,32 @@ function player.update(dt)
   local w = love.graphics.getWidth()
   local h = love.graphics.getHeight()
 
-  if cam.x < w/2 then
-    cam.x = w/2
+  if cam.x < w / 2 then
+    cam.x = w / 2
   end
 
-  if cam.y < h/2 then
-    cam.y = h/2
+  if cam.y < h / 2 then
+    cam.y = h / 2
   end
 
   local mapW = gameMap.map.width * gameMap.map.tilewidth
   local mapH = gameMap.map.height * gameMap.map.tileheight
 
-  if cam.x > (mapW - w/2) then
-    cam.x = (mapW - w/2)
+  if cam.x > (mapW - w / 2) then
+    cam.x = (mapW - w / 2)
   end
 
-  if cam.y > (mapH - h/2) then
-    cam.y = (mapH - h/2)
+  if cam.y > (mapH - h / 2) then
+    cam.y = (mapH - h / 2)
   end
-  
+
 end
 
 function player.draw()
   cam:attach()
-    gameMap.draw()
-    player.anim:draw(player.spriteSheet, player.x, player.y, nil, 2, nil, 32, 32) --posx, posy, nil-> no rotation, scale x factor-> y wil also adopt that effect
-    --offset of camera must take half of width and half of height of sprite (to go directly in the center)
+  gameMap.draw()
+  player.anim:draw(player.spriteSheet, player.x, player.y, nil, 2, nil, 32, 32) -- posx, posy, nil-> no rotation, scale x factor-> y wil also adopt that effect
+  -- offset of camera must take half of width and half of height of sprite (to go directly in the center)
   cam:detach()
 end
 
