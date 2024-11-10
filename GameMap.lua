f23a02093b13c9fa918dce89459a9cbab3f8857e
Nes <147901220+IzNescafe@users.noreply.github.com>
2024-11-10 15:52:09 +0630

    moved the walls collision to the gameMap

--- GameMap.lua
+++ GameMap.lua
@@ -3,15 +3,25 @@ gameMap = {}
 function gameMap.load()
     
     wf = require 'libraries/windfield'
-    world = wf.newWorld(0, 0)
+    gameMap.world = wf.newWorld(0, 0)
 
     sti = require 'libraries/sti'
     gameMap.map = sti('maps/ground-floor.lua')
 
+    walls = {}
+    if gameMap.map.layers["walls"] then
+    for i, obj in pairs(gameMap.map.layers["walls"].objects) do
+      local wall = gameMap.world:newRectangleCollider(obj.x, obj.y, obj.width, obj.height)
+      wall:setType('static')
+      table.insert(walls, wall)
+    end
+  end
+
 end
 
--- function gameMap.update(dt)
--- end
+function gameMap.update(dt)
+    gameMap.world:update(dt)
+end
 
 function gameMap.draw()
     gameMap.map:drawLayer(gameMap.map.layers["main"])
