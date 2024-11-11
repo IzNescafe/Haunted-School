233f3007b8c5c25dd1c2d093545033cc8cb063a3
Nes <147901220+IzNescafe@users.noreply.github.com>
2024-11-11 12:19:06 +0630

    i made some major changes
    moved the camera here so we can add more entites in the game
    added the enemy functions

--- playing.lua
+++ playing.lua
@@ -1,24 +1,62 @@
 playing = {}
 
 local player = require("Player")
--- local enemy = require("enemy")
+local enemy = require("enemy")
 local timer = require("Timer")
+local gameMap = require("GameMap")
 
 function playing.load()
+
+    camera = require 'libraries/camera'
+    cam = camera()
+
+    gameMap.load()
     player.load()
-    -- enemy.load()
+    enemy.load()
     timer.load()
 end
 
 function playing.update(dt)
-    timer.update(dt)
     player.update(dt)
+    enemy.update(dt)
+    timer.update(dt)
+    gameMap.update(dt) --update physics world
+
+    cam:lookAt(player.x, player.y)
+
+    -- only use after resizing sprite and map
+  
+    local w = love.graphics.getWidth()
+    local h = love.graphics.getHeight()
+  
+    if cam.x < w/2 then
+      cam.x = w/2
+    end
+  
+    if cam.y < h/2 then
+      cam.y = h/2
+    end
+  
+    local mapW = gameMap.map.width * gameMap.map.tilewidth
+    local mapH = gameMap.map.height * gameMap.map.tileheight
+  
+    if cam.x > (mapW - w/2) then
+      cam.x = (mapW - w/2)
+    end
+  
+    if cam.y > (mapH - h/2) then
+      cam.y = (mapH - h/2)
+    end
 end
 
 function playing.draw()
-    player.draw()
-    timer.draw()
-    -- enemy.draw()
+    cam:attach()
+      gameMap.draw()
+      timer.draw()
+      player.draw()
+      enemy.draw()
+    --   gameMap.world:draw() --to check the colliders/hitboxes of the world
+  cam:detach()
 end
 
 return playing
